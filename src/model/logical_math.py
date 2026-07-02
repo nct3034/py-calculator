00dd7f35f7923fd2630e3ca593f15e071d9d1baa
Thanh <ncthanh3034@gmail.com>
2026-07-02 10:12:56 +0700

    Logical Math

---
+++ src/model/logical_math.py
@@ -0,0 +1,12 @@
+class LogicalMath:
+    def logical_and(self, a: bool, b: bool) -> bool:
+        return a and b
+
+    def logical_or(self, a: bool, b: bool) -> bool:
+        return a or b
+
+    def logical_not(self, a: bool) -> bool:
+        return not a
+
+    def logical_xor(self, a: bool, b: bool) -> bool:
+        return a ^ b
